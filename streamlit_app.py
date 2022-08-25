503fef3f261b57f8dce1584a0d7ae12dc43c7fd9
Ronald Gunther <rongunther@gmail.com>
2022-08-25 13:02:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,11 @@ streamlit.dataframe(fruits_to_show)
 
 # REST API Calls
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
 # normalize the json response payload 
